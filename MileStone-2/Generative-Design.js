a1bb6c8a1651b6428a00b5a1e66b4c0da280980e
Hamza El Aisati <hamza.el.aisati@student.ehb.be>
2024-11-10 20:30:31 +0100

    Update (if else + for) voor de bovenste rechthoeken

--- MileStone-2/Generative-Design.js
+++ MileStone-2/Generative-Design.js
@@ -4,18 +4,57 @@ import * as Utils from "../scripts/utils.js";
 //variabelen
 let width = context.canvas.width;
 let height = context.canvas.height;
+let random, x,y, widthRect, 
+heightRect = height /25;
 
 //uitvoer
 drawRectangle(); //functie "drawRectangle" opgeroept
 
 //verwerking
 function drawRectangle() {
-    context.fillStyle = "#29ABE2"; //blauw
-    context.fillRect(width - width / 1.34,height - height + height / 35, width- width / 1.93, height / 25);
-    context.fillStyle = "#ED1C24"; //rood
-    context.fillRect(width - width / 1.2,height - height + height / 10, width- width / 2.9, height / 25);
-    context.fillStyle = "#FFC700"; //geel
-    context.fillRect(width - width / 1.09 ,height - height + height / 5.8, width- width / 5.7, height / 25);
-    context.fillStyle = "black"; //zwart
-    context.fillRect(0,height - height + height / 4.1, width, height / 25);
+    //Bovenste rechthoeken
+    for(let i = 0; i<4; i++)
+    {
+        for (let j = 0; j <2;j++)
+        {
+
+            if (i == 0) {
+                x = width - width / 1.35;
+                y = height / 35;
+                widthRect = width- width / 1.9;
+                
+                context.fillStyle = "#29ABE2"; //blauw
+                context.fillRect(x,y,widthRect,heightRect);
+            }
+            else if (i == 1) {
+                x = width - width / 1.2;
+                y = height / 10;
+                widthRect = width- width / 2.9;
+                
+                context.fillStyle = "#ED1C24"; //rood
+                context.fillRect(x,y,widthRect,heightRect);
+            }
+            else if (i == 2) {
+                x = width - width / 1.09;
+                y = height / 5.8;
+                widthRect = width- width / 5.9;
+
+                context.fillStyle = "#FFC700"; //geel
+                context.fillRect(x,y,widthRect,heightRect);
+            }
+            else {
+                x = 0;
+                y = height / 4.1;
+                widthRect = width;
+
+                context.fillStyle = "black"; //zwart
+                context.fillRect(x,y,widthRect,heightRect);
+            }
+        }
+    }
+        
+      
+
+    //onderste rechthoeken 
+
 }

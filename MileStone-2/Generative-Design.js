dd4daaaeb3c13d574cf62cc6c412ba0467102efb
Hamza El Aisati <hamza.el.aisati@student.ehb.be>
2024-11-12 00:39:41 +0100

    Meer commentaars bijgevoegd

--- MileStone-2/Generative-Design.js
+++ MileStone-2/Generative-Design.js
@@ -1,5 +1,7 @@
+//scripts
 import context from "../scripts/context.js";
 import * as Utils from "../scripts/utils.js";
+
 
 //variabelen
 let width = context.canvas.width;
@@ -7,10 +9,13 @@ let height = context.canvas.height;
 let random, x,x2, x3,y2, y, widthRect, 
 heightRect = height /25;
 
-//uitvoer
-Background();
+
+//uitvoer (oproep van de verschillende functies)
+Background(); 
 drawSpecialLines();
 drawLines();
+drawDiagonalLines();
+
 
 //verwerking
 function Background() {
@@ -36,57 +41,57 @@ function drawLines() {
 
         Utils.drawLine(width / 2, height / 2, x2, height); // tekent de lijnen op de onderste helft van het scherm
         i++;
-    } while (x2 < width); //while stopt wanneer x2 kleiner is dan de width
+    } while (x2 < width); //stopt wanneer x2 groter is dan de helft van de width
 }
 
 
 function drawSpecialLines(){
     let i =0;
     do{
-        random = Utils.randomNumber(1,3);
+        random = Utils.randomNumber(1,3); //random nummer tussen 1 en 3
         if(random == 1){
-            context.strokeStyle = Utils.hsla(197, 76, 53, 100);
+            context.strokeStyle = Utils.hsla(197, 76, 53, 100); //blauw
         }
         else if(random == 2) {
-            context.strokeStyle = Utils.hsla(358, 87, 50, 100);
+            context.strokeStyle = Utils.hsla(358, 87, 50, 100); //rood
         }
         else {
-            context.strokeStyle = Utils.hsla(47, 100, 50, 100);
+            context.strokeStyle = Utils.hsla(47, 100, 50, 100); //geel
         }
 
-        x2 = width / 2 * i / 10;
-        x3 = width - width / 2 * i / 10; 
-        y2 = height / 10 * i;            
-    
-        Utils.drawLine(0, y2, x2, height - height / 20 * i);
-        Utils.drawLine(width, y2, x3, height - height / 20 * i);
+        x2 = width / 2 * i / 10; //x-coordinaat voor de linkse speciale lijnen
+        x3 = width - width / 2 * i / 10; //x-coordinaat voor de rechtse speciale lijnen
+        y2 = height / 10 * i; //y-coordinaat            
+        
+        // console.log(x2 + " " + x3 + " " + y2); //om mijn coordinaten op de console de bekijken
+
+        Utils.drawLine(0, y2, x2, height - height / 20 * i); //tekent de linkse speciale lijnen op de scherm 
+        Utils.drawLine(width, y2, x3, height - height / 20 * i); //tekent de rechtse speciale lijnen op de scherm
 
         i++;
-    }while(x2 < width/2);
+    }while(x2 < width/2); //stopt wanneer x2 groter is dan de helft van de width
 }
 
-drawDiagonalLines();
 
 function drawDiagonalLines() {
-    context.lineWidth = 5;
-    context.lineCap = "round";
+    context.lineWidth = 5; //lijn dikte
+    context.lineCap = "round"; //zorgt ervoor dat de hoekjes van de lijn "rond" worden
+
     for(let i = 0; i < 2; i++)
         {
-            random = Utils.randomNumber(1,3);
+            random = Utils.randomNumber(1,3); //random nummer tussen 1 en 3
 
             if(random == 1){
-                context.strokeStyle = Utils.hsla(197, 76, 53, 100);
+                context.strokeStyle = Utils.hsla(197, 76, 53, 100); //blauw
             }
             else if(random == 2) {
-                context.strokeStyle = Utils.hsla(358, 87, 50, 100);
+                context.strokeStyle = Utils.hsla(358, 87, 50, 100); //rood
             }
             else {
-                context.strokeStyle = Utils.hsla(47, 100, 50, 100);
+                context.strokeStyle = Utils.hsla(47, 100, 50, 100); //geel
             }
 
-            Utils.drawLine(0,i * height,width/2, height/2);
-            Utils.drawLine(width -1,i * height,width/2, height/2);   
+            Utils.drawLine(0,i * height,width/2, height/2); //zorgt voor de twee lijnen aan de linker kant van het scherm
+            Utils.drawLine(width -1,i * height,width/2, height/2); //zorgt voor de twee lijnen aan de rechter kant van het scherm
         }
-    
-    
 }

84bb1ee4f8cd0b79dac931130d15aea4a6248004
Hamza El Aisati <hamza.el.aisati@student.ehb.be>
2024-11-11 23:58:40 +0100

    Ander design (met speciale lijnen)

--- MileStone-2/Generative-Design.js
+++ MileStone-2/Generative-Design.js
@@ -4,115 +4,88 @@ import * as Utils from "../scripts/utils.js";
 //variabelen
 let width = context.canvas.width;
 let height = context.canvas.height;
-let random, x,x2, y, widthRect, 
+let random, x,x2, x3,y2, y, widthRect, 
 heightRect = height /25;
 
 //uitvoer
 Background();
+drawTest();
 drawLines();
-drawRectangle(); //functie "drawRectangle" opgeroept
 //verwerking
 
 function Background() {
     context.fillRect(0,0, width, height);
 }
-function drawRectangle() {
-    //Bovenste- onderste rechthoeken 
-    for(let i = 0; i<4; i++)
-    {
-        for (let j = 0; j <2;j++)
-        {
-            if (i == 0) {
-                x = width - width / 1.35;
-                y = height / 35; //y-coord voor boven
-                widthRect = width- width / 1.9;
-                if(j == 1) {
-                    y = height / 1.07; //y-coord voor beneden
-                }
-                context.fillStyle = "#29ABE2"; //blauw
-                context.fillRect(x,y,widthRect,heightRect);
-            }
-            else if (i == 1) {
-                x = width - width / 1.2;
-                y = height / 10; //y-coord voor boven
-                widthRect = width- width / 2.9;
-                if(j == 1) {
-                    y = height / 1.16; //y-coord voor beneden
-                }
-                context.fillStyle = "#ED1C24"; //rood
-                context.fillRect(x,y,widthRect,heightRect);
-            }
-            else if (i == 2) {
-                x = width - width / 1.09;
-                y = height / 5.8; //y-coord voor boven
-                widthRect = width- width / 5.9;
-                if(j == 1) {
-                    y = height / 1.27; //y-coord voor beneden
-                }
-                context.fillStyle = "#FFC700"; //geel
-                context.fillRect(x,y,widthRect,heightRect);
-            }
-            else {
-                x = 0;
-                y = height / 4.1; //y-coord voor boven
-                widthRect = width;
-                if(j == 1) {
-                    y = height / 1.4; //y-coord voor beneden
-                }
-                context.fillStyle = "black"; //zwart
-                context.fillRect(x,y,widthRect,heightRect);
-            }
-        }
-    }
-}
 
 
 function drawLines() {
-    for(let i = 0; i < 385; i++)
-    {
-        x2 = i * 5;
-        random = Utils.randomNumber(0,3);
-        if(random == 0){
+    let i = 0;
+    do {
+        x2 = i * 30;
+        random = Utils.randomNumber(0, 3);
+        
+        if (random == 0) {
             context.strokeStyle = "#29ABE2";
-        }
-        else if(random == 1) {
+        } else if (random == 1) {
             context.strokeStyle = "#ED1C24"; 
-        }
-        else if(random == 2){
+        } else if (random == 2) {
             context.strokeStyle = "#FFC700"; 
-        }
-        else{
+        } else {
             context.strokeStyle = "black";
         }
-        Utils.drawLine(width / 2, height / 3.6, x2, 0);
-        Utils.drawLine(width / 2, height / 1.36, x2, height);
-
-
-
-        context.strokeStyle = "black";
-        Utils.drawLine(width / 2, height /2, i * 5, 0);
-        Utils.drawLine(width / 2, height /2, i * 5, height);
-        Utils.drawLine(width / 2, height /2, 0, i * 2.46);
-        Utils.drawLine(width / 2, height /2, width, i * 2.46);
-    }
+        Utils.drawLine(width / 2, height / 2, x2, 0);
+        Utils.drawLine(width / 2, height / 2, x2, height);
+        i++;
+    } while (x2 < width);
 }
 
-drawTest();
 
 function drawTest(){
-    for(let i = 0; i < 50; i++)
-    {
+    let i =0;
+    do{
         random = Utils.randomNumber(1,3);
         if(random == 1){
-            context.fillStyle = Utils.hsla(197, 76, 53, 100-i*2);
+            context.strokeStyle = Utils.hsla(197, 76, 53, 100);
         }
         else if(random == 2) {
-            context.fillStyle = Utils.hsla(358, 87, 50, 100-i*2);
+            context.strokeStyle = Utils.hsla(358, 87, 50, 100);
         }
         else {
-            context.fillStyle = Utils.hsla(47, 100, 50, 100-i*2);
+            context.strokeStyle = Utils.hsla(47, 100, 50, 100);
         }
-           
-        context.fillRect(0 + 50 * i,height / 4, 40, height /2);
-    }
+
+        x2 = width / 2 * i / 10;
+        x3 = width - width / 2 * i / 10; 
+        y2 = height / 10 * i;            
+    
+        Utils.drawLine(0, y2, x2, height - height / 20 * i);
+        Utils.drawLine(width, y2, x3, height - height / 20 * i);
+
+        i++;
+    }while(x2 < width/2);
+}
+
+drawLineTest();
+
+function drawLineTest() {
+    context.lineWidth = 5;
+    context.lineCap = "round";
+    for(let i = 0; i < 2; i++)
+        {
+            random = Utils.randomNumber(1,3);
+            if(random == 1){
+                context.strokeStyle = Utils.hsla(197, 76, 53, 100);
+            }
+            else if(random == 2) {
+                context.strokeStyle = Utils.hsla(358, 87, 50, 100);
+            }
+            else {
+                context.strokeStyle = Utils.hsla(47, 100, 50, 100);
+            }
+
+            Utils.drawLine(0,i * height,width/2, height/2);
+            Utils.drawLine(width -1,i * height,width/2, height/2);   
+        }
+    
+    
 }

bc2e2bcbbac90f5f4791843542baaad9d555f2f7
Hamza El Aisati <hamza.el.aisati@student.ehb.be>
2024-11-11 17:32:10 +0100

    Nieuw concept?

--- MileStone-2/Generative-Design.js
+++ MileStone-2/Generative-Design.js
@@ -4,13 +4,18 @@ import * as Utils from "../scripts/utils.js";
 //variabelen
 let width = context.canvas.width;
 let height = context.canvas.height;
-let random, x,y, widthRect, 
+let random, x,x2, y, widthRect, 
 heightRect = height /25;
 
 //uitvoer
+Background();
+drawLines();
 drawRectangle(); //functie "drawRectangle" opgeroept
-
 //verwerking
+
+function Background() {
+    context.fillRect(0,0, width, height);
+}
 function drawRectangle() {
     //Bovenste- onderste rechthoeken 
     for(let i = 0; i<4; i++)
@@ -59,9 +64,55 @@ function drawRectangle() {
             }
         }
     }
-        
-      
+}
 
-    //onderste rechthoeken 
 
+function drawLines() {
+    for(let i = 0; i < 385; i++)
+    {
+        x2 = i * 5;
+        random = Utils.randomNumber(0,3);
+        if(random == 0){
+            context.strokeStyle = "#29ABE2";
+        }
+        else if(random == 1) {
+            context.strokeStyle = "#ED1C24"; 
+        }
+        else if(random == 2){
+            context.strokeStyle = "#FFC700"; 
+        }
+        else{
+            context.strokeStyle = "black";
+        }
+        Utils.drawLine(width / 2, height / 3.6, x2, 0);
+        Utils.drawLine(width / 2, height / 1.36, x2, height);
+
+
+
+        context.strokeStyle = "black";
+        Utils.drawLine(width / 2, height /2, i * 5, 0);
+        Utils.drawLine(width / 2, height /2, i * 5, height);
+        Utils.drawLine(width / 2, height /2, 0, i * 2.46);
+        Utils.drawLine(width / 2, height /2, width, i * 2.46);
+    }
+}
+
+drawTest();
+
+function drawTest(){
+    for(let i = 0; i < 50; i++)
+    {
+        random = Utils.randomNumber(1,3);
+        if(random == 1){
+            context.fillStyle = Utils.hsla(197, 76, 53, 100-i*2);
+        }
+        else if(random == 2) {
+            context.fillStyle = Utils.hsla(358, 87, 50, 100-i*2);
+        }
+        else {
+            context.fillStyle = Utils.hsla(47, 100, 50, 100-i*2);
+        }
+           
+        context.fillRect(0 + 50 * i,height / 4, 40, height /2);
+    }
 }
